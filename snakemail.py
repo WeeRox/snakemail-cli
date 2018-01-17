047ff0e594bebffa9aae2c9a5d8158d8e77a3625
Aron Bergman <bathingrad@gmail.com>
2018-01-17 14:01:11 +0100

    Added support for login command

--- snakemail.py
+++ snakemail.py
@@ -3,6 +3,7 @@ usage: snakemail <command> [<args>...]
 
 useful commands:
     account  handles connections to email accounts
+    login    logs you in to your email account
 """
 
 import docopt
@@ -13,6 +14,9 @@ arguments = docopt.get_args()
 if arguments['<command>'] == 'account':
     from command import snakemail_account
     snakemail_account.run(arguments)
+elif arguments['<command>'] == 'login':
+    from command import snakemail_login
+    snakemail_login.run(arguments)
 else:
     exit('snakemail: %r is not a snakemail command. See \'snakemail --help\'.' % arguments['<command>'])
 
