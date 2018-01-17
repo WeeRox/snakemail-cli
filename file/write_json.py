9cbfb55ef7a4cd8826b0fe322abe597ee73697cf
Aron Bergman <bathingrad@gmail.com>
2018-01-17 14:00:31 +0100

    Created method to change auto login account

--- file/write_json.py
+++ file/write_json.py
@@ -4,13 +4,19 @@ import sys, os
 def create():
     file = open(os.path.join(os.path.dirname(sys.argv[0]), ".config"), "w")
     json_data = {
-        "accounts": []
+        "accounts": [],
+        "auto_login": null
     }
     json.dump(json_data, file)
 
 def add_account(email, password, host):
     json_data, file = config_file()
     json_data['accounts'].append({"email": email, "password": password, "host": host})
+    json.dump(json_data, file)
+
+def set_auto_login(email):
+    json_data, file = config_file()
+    json_data['auto_login'] = email
     json.dump(json_data, file)
 
 def config_file():
