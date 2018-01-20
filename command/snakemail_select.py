884fe92487fb2168879444c7bad6050750a13499
Aron Bergman <bathingrad@gmail.com>
2018-01-20 12:37:34 +0100

    Fixed error in select command

--- command/snakemail_select.py
+++ command/snakemail_select.py
@@ -4,13 +4,20 @@ usage: snakemail select [<mailbox>]
 
 from docopt import docopt
 from mail import imap
+from file import read_json, write_json
 
 def run(argv):
     arguments = docopt(__doc__, argv=argv)
-    mailbox = arguments['<mailbox>']
-    if mailbox:
-        status, count = imap.select(mailbox)
+    if read_json.get_auto_login():
+        mailbox = arguments['<mailbox>']
+        if mailbox:
+            status, count = imap.select(mailbox)
+            write_json.set_auto_mailbox(mailbox)
+        else:
+            status, count = imap.select()
+            write_json.set_auto_mailbox('INBOX')
+            
+        if status == 'NO':
+            exit("snakemail: there was a problem selecting that inbox, make sure you haven't misspelled the name")
     else:
-        status, count = imap.select()
-    if status == 'NO':
-        exit("snakemail: there was a problem selecting that inbox, make sure you haven't misspelled the name")
+        exit("snakemail: you are not logged in! To log in, run 'snakemail login <email>'")
